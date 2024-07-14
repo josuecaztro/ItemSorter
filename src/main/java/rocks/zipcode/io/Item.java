0707a92efe4a51f10133a925de79ff2bcbba59c4
Josue <josue@zipcodersmbp10.lan>
2024-07-14 14:08:44 -0400

    AlmostDone

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,45 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
+
+    private String name;
+    private Long id;
+    private Double price;
+
     public Item() {
+        this.name = "";
+        this.id = 0L;
+        this.price = 0.01;
     }
 
     public Item(Long id, String name, Double price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Long getId() {
-        return null;
+        return this.id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public Double getPrice() {
-        return null;
+
+        return this.price;
     }
 
     public void setPrice(Double price) {
+        this.price = price;
     }
 }
--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,25 @@
 package rocks.zipcode.io;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+
+    private Item[] items;
+
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        ArrayList<Item> myList = new ArrayList<>(Arrays.asList(items));
+        myList.sort(Comparator.comparing(Item::getId));
+        Item[] solution = myList.toArray(new Item[0]);
+        return solution;
     }
+
 }
